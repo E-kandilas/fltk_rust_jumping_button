8f40ed399e4c9625cd855c03a086398c45cfc699
Emmanuel Kandilas <ekandilas@gmail.com>
2024-09-19 14:49:17 -0400

    update the readme and clean up the code/ make the button and stuff more attractive

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
-The jumping button is a Rust application built using the FLTK library. It is a graphical user interface that displays a button which jumps to a random position when clicked.
+The jumping button is a Rust application built using the FLTK library. It is a GUI that displays a button which jumps to a random position when clicked.
+To run the jumping button application, you have two options: building and running it one time, or using hot module replacement (HMR) with Cargo Watch for development.
 
-To run the jumping button application with hot module replacement (HMR) using Cargo Watch, follow these steps:
+## Building and Running the Application
+
+1. Open your terminal and navigate to the project directory where the `Cargo.toml` file is located.
+
+2. Run the following command to build the application:
+  ```
+  cargo build
+  ```
+
+3. Once the build is complete, run the application with:
+  ```
+  cargo run
+  ```
+
+This will compile and run the application one time. You can interact with the jumping button application and observe its behavior as you click the button.
+
+## Developing with Hot Module Replacement (HMR)
+
+To develop the jumping button application with HMR using Cargo Watch, follow these steps:
 
 1. Make sure you have Cargo Watch installed. If not, you can install it by running `cargo install cargo-watch` in your terminal.
 
@@ -11,6 +30,6 @@ To run the jumping button application with hot module replacement (HMR) using Ca
   cargo watch -x run
   ```
 
-4. Cargo Watch will automatically rebuild and restart the application whenever changes are detected in the source code. This allows you to see the changes in real-time without manually restarting the application.
+Cargo Watch will automatically rebuild and restart the application whenever changes are detected in the source code. This allows you to see the changes in real-time without manually restarting the application.
 
 That's it! You can now interact with the jumping button application and observe its behavior as you click the button. Enjoy!
--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,69 @@
-
 use fltk::{prelude::*, button::Button, enums::{Color, FrameType}, app, *};
 use fltk::enums::Event;
-use frame::Frame;
 use rand::Rng;
 
 // Define the Message type
 #[derive(Debug, Clone, Copy)]
 enum Message {
-    Spaz,
+  Spaz,
 }
 
+// Constants for centralized variables
+const WIN_WIDTH: i32 = 600;
+const WIN_HEIGHT: i32 = 400;
+const BUTTON_WIDTH: i32 = 100;
+const BUTTON_HEIGHT: i32 = 50;
+const BUTTON_LABEL: &str = "Click me!";
+
 fn main() {
-    let app = app::App::default().with_scheme(fltk::app::Scheme::Gleam);
+  let app = app::App::default().with_scheme(fltk::app::Scheme::Gleam);
 
-    let mut win = fltk::window::Window::new(100, 100, 400, 300, "ClickyButton");
-    let mut frame = fltk::frame::Frame::new(0, 0, 400, 300, "");
-    frame.set_color(fltk::enums::Color::White);
+  let mut win = fltk::window::Window::new(100, 100, WIN_WIDTH, WIN_HEIGHT, "ClickyButton");
+  let mut frame = fltk::frame::Frame::new(0, 0, WIN_WIDTH, WIN_HEIGHT, "");
+  frame.set_color(fltk::enums::Color::White);
 
-    let (sender, reciever) = app::channel::<Message>();
+  let (sender, receiver) = app::channel::<Message>();
 
-    let mut frame = Frame::new(0, 0, 400, 300, "");
-    frame.set_color(fltk::enums::Color::White);
-    let mut but = Button::new(160, 200, 80, 40, "Click me!");
-    but.set_color(Color::Cyan);
-    but.set_label_color(Color::Red);
-    but.set_frame(FrameType::FlatBox);
-    but.set_label_size(20);
+  let mut but = Button::new((WIN_WIDTH - BUTTON_WIDTH) / 2, WIN_HEIGHT - BUTTON_HEIGHT - 20, BUTTON_WIDTH, BUTTON_HEIGHT, BUTTON_LABEL);
+  but.set_color(Color::Cyan);
+  but.set_label_color(Color::Red);
+  but.set_frame(FrameType::RoundUpBox);
+  but.set_label_size(20);
+  but.set_selection_color(Color::Green);
 
-    but.handle(move |b, evt| match evt {
-        Event::Enter => {
-          b.set_color(Color::XtermBgYellow);
-          b.redraw();
-          return true;
-        }
-        Event::Leave => {
-          b.set_color(Color::Cyan);
-          b.redraw();
-          return true;
-        }
-        Event::Push => {
-            b.emit(sender, Message::Spaz);
-            return true;
-        }
-        _ => {
-          return false;
-        },
-    });
+  but.handle(move |b, evt| match evt {
+    Event::Enter => {
+      b.set_color(Color::XtermBgYellow);
+      b.redraw();
+      true
+    }
+    Event::Leave => {
+      b.set_color(Color::Cyan);
+      b.redraw();
+      true
+    }
+    Event::Push => {
+      b.emit(sender, Message::Spaz);
+      true
+    }
+    _ => false,
+  });
 
-    win.end();
-    win.show();
+  win.end();
+  win.show();
 
-    while app.wait() {
-      if let Some(msg) = reciever.recv() {
-        match msg {
-          Message::Spaz => {
-            let rand_x = rand::thread_rng().gen_range(0..(400 - 80));
-            let rand_y = rand::thread_rng().gen_range(0..100);
-            but.resize(rand_x, rand_y, 80, 40);
-            win.redraw();
-          }
+  while app.wait() {
+    if let Some(msg) = receiver.recv() {
+      match msg {
+        Message::Spaz => {
+          let rand_x = rand::thread_rng().gen_range(0..(WIN_WIDTH - BUTTON_WIDTH));
+          let rand_y = rand::thread_rng().gen_range(0..(WIN_HEIGHT - BUTTON_HEIGHT));
+          but.resize(rand_x, rand_y, BUTTON_WIDTH, BUTTON_HEIGHT);
+          win.redraw();
         }
       }
     }
+  }
 
-    app.run().unwrap();
+  app.run().unwrap();
 }
